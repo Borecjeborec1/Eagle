44c31deb3d66a3b37145582c0715cb1ef731041e
borecjeborec1 <robingrundel@seznam.cz>
2022-04-14 20:38:15 +0200

    transparent screen

--- Eagle/client/scripts/script.js
+++ Eagle/client/scripts/script.js
@@ -1,15 +1,22 @@
 const declineBtn = document.querySelector('.decline');
 const acceptBtn = document.querySelector('.accept');
 const bg = document.querySelector('.bg');
+const timerHeading = document.querySelector('#timer');
 declineBtn.addEventListener("mouseover", () => {
   bg.style.backgroundImage = "url(../assets/eagle-red-both.png)";
 })
 declineBtn.addEventListener("mouseout", () => {
   bg.style.backgroundImage = "url(../assets/eagle.png)";
 })
-acceptBtn.addEventListener("mouseover", () => {
-  bg.style.backgroundImage = "url(../assets/eagle-green-both.png)";
-})
-acceptBtn.addEventListener("mouseout", () => {
-  bg.style.backgroundImage = "url(../assets/eagle.png)";
-})
+
+
+timerHeading.style.opacity = 0;
+setTimeout(() => {
+  let timeToAccept = 30
+  timerHeading.style.opacity = 1;
+  let timerInt = setInterval(() => {
+    timerHeading.innerText = `Accept in ${timeToAccept}s`
+    timeToAccept--
+    if (timeToAccept < 0) clearInterval(timerInt)
+  }, 1000)
+}, 30000)
--- Eagle/client/scripts/setup.js
+++ Eagle/client/scripts/setup.js
@@ -1,6 +1,11 @@
+const SMALL_WIDTH = 400
+const SMALL_HEIGHT = 200
+const TAURI_WINDOW = window.__TAURI__.window
 async function main() {
-  let monitor = await window.__TAURI__.window.primaryMonitor()
-  window.__TAURI__.window.appWindow.setPosition(new window.__TAURI__.window.LogicalPosition((monitor.size.width - 400) / 2, (monitor.size.height - 200) / 2))
+
+  let monitor = await TAURI_WINDOW.primaryMonitor()
+  TAURI_WINDOW.appWindow.setSize(new TAURI_WINDOW.LogicalSize(SMALL_WIDTH, SMALL_HEIGHT))
+  TAURI_WINDOW.appWindow.setPosition(new TAURI_WINDOW.LogicalPosition((monitor.size.width - 400) / 2, (monitor.size.height - 200) / 2))
 }
 
 main()
