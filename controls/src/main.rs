77e5d4bf58be86f5394af55e451447200eb54ba5
borecjeborec1 <robingrundel@seznam.cz>
2022-04-24 13:55:12 +0200

    controls functionality

--- controls/src/main.rs
+++ controls/src/main.rs
@@ -3,19 +3,37 @@ use std::process::Command;
 
 fn main() {
     const MINUTES_INTERVAL: u64 = 30;
-    const POSTPONE_TIME: u64 = 2;
     const UI_PATH: &str = "../Eagle/src-tauri/target/debug/app.exe";
     const CONFIG_PATH: &str = "../config/config.json";
     loop {
-        let file_before =
-            fs::read_to_string(CONFIG_PATH).expect("Failed to open config file before ui");
-        Command::new(UI_PATH).spawn().expect("Failed to spawn ui");
-        std::thread::sleep(minutes(POSTPONE_TIME));
-        let file_after =
-            fs::read_to_string(CONFIG_PATH).expect("Failed to open config file after ui");
-        println!("{}", file_before);
-        println!("{}", file_after);
         std::thread::sleep(minutes(MINUTES_INTERVAL));
+        loop {
+            let file_before = fs::read_to_string(CONFIG_PATH)
+                .expect("Failed to open config file before ui")
+                .parse::<serde_json::Value>()
+                .unwrap();
+
+            Command::new(UI_PATH).spawn().expect("Failed to spawn ui");
+            std::thread::sleep(minutes(
+                file_before["waitTime"].to_string().parse::<u64>().unwrap(),
+            ));
+
+            let file_after = fs::read_to_string(CONFIG_PATH)
+                .expect("Failed to open config file after ui")
+                .parse::<serde_json::Value>()
+                .unwrap();
+            if file_before["postponed"] != file_after["postponed"] && file_after["postponed"] != "0"
+            {
+                std::thread::sleep(minutes(
+                    file_after["postponeTime"]
+                        .to_string()
+                        .parse::<u64>()
+                        .unwrap(),
+                ));
+            } else {
+                break;
+            }
+        }
     }
 }
 
